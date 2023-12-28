38b6c2c89761f36257e45c272a4cdf137d1a7c48
Siméon zilu <50544380+ExcaliburSims@users.noreply.github.com>
2023-12-28 14:11:09 +0100

    feat: add function for use dnd kit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,65 @@
-import './App.css'
+import { arrayMove, sortableKeyboardCoordinates } from "@dnd-kit/sortable";
+import "./App.css";
+import { DndContext, DragEndEvent, DragStartEvent, KeyboardSensor, PointerSensor, TouchSensor, closestCenter, useSensor, useSensors } from "@dnd-kit/core";
+import { useState } from "react";
+import { ImageGallery } from "./types/global.types";
+import { initialImageData } from "./data";
 
 function App() {
+  const [galleryData, setGalleryData] = useState(initialImageData)
+  // dnd code start here
+
+  const [activeItem, setActiveItem] = useState<ImageGallery | null>(null)
+  const sensors = useSensors(
+    useSensor(PointerSensor), 
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    }),
+    useSensor(TouchSensor)
+  );
+  const handleDragStart = (event: DragStartEvent ) => {
+    const { id } = event.active;
+
+    if (!id) return;
+
+    const currentItem = galleryData.find((item) => item.id === id);
+
+    setActiveItem(currentItem || null);
+  };
+  const handleDragEnd = (event: DragEndEvent) => {
+    setActiveItem(null);
+    const { active, over } = event;
+
+    if (!over) {
+      return;
+    }
+
+    if (active.id !== over.id) {
+      setGalleryData((items) => {
+        const oldIndex = items.findIndex((item) => item.id === active.id);
+        const newIndex = items.findIndex((item) => item.id === over.id);
+
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    }
+  };
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello Developers</h1>
-    </>
-  )
+    <div className="min-h-screen">
+      <div className="container flex flex-col items-center">
+        <div className="grid max-w-5xl my-8 bg-white divide-y rounded-lg shadow">
+          <header className="text-2xl">Showcase</header>
+          <DndContext
+            sensors={sensors}
+            collisionDetection={closestCenter}
+            onDragStart={handleDragStart}
+            onDragEnd={handleDragEnd}
+          >
+            <div>Image gallery</div>
+          </DndContext>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
